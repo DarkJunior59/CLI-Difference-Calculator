2a9304c5c41a0c674e3c3d483c6aa8fcb47e49b4
DarkJunior <sfp87@protonmail.com>
2021-04-06 20:35:58 +0300

    add logic programm difference between two files

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,12 +2,15 @@
 
 import program from 'commander';
 
+import genDiff from '../cli/filecomparison.js';
+
 program
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
   .helpOption('-h, --help', 'output usage information')
   .arguments('<filepath1> <filepath2>')
-  .option('-f, --format [type]',  'output format')
+  .option('-f, --format [type]', 'output format')
+  .action((filepath1, filepath2) => console.log(genDiff(filepath1, filepath2)))
   .parse(process.argv);
 
 export default program;
