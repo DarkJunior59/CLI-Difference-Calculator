b6ffbeed6ce68f0c047b17ac72a29494ac709cef
DarkJunior <sfp87@protonmail.com>
2021-04-22 18:16:35 +0300

    add tests for two flat yaml files

--- __tests__/testGendiff.js
+++ __tests__/testGendiff.js
@@ -7,7 +7,14 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const getPathFile = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-test('diffirence with two flat files', () => {
-  expect(genDiff(getPathFile('file1.json'), getPathFile('file2.json')))
+test('diffirence with two flat json files', () => {
+  expect(genDiff(getPathFile('filepath1.json'), getPathFile('filepath2.json')))
+    .toEqual(result);
+});
+
+test('diffirence with two flat yaml files', () => {
+  expect(genDiff(getPathFile('filepath1.yaml'), getPathFile('filepath2.yaml')))
+    .toEqual(result);
+  expect(genDiff(getPathFile('filepath1.yml'), getPathFile('filepath2.yml')))
     .toEqual(result);
 });
