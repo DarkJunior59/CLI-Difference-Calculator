011ec94a8c7a3f528bdf0e133b225d9b9a62cbe9
DarkJunior <sfp87@protonmail.com>
2021-06-20 10:23:11 +0300

    add tests for nested structure

--- __tests__/testGendiff.js
+++ __tests__/testGendiff.js
@@ -1,20 +1,30 @@
 import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
-import result from '../__fixtures__/result.js';
-import genDiff from '../cli/filecomparison.js';
+import fs from 'fs';
+import genDiff from '../cli/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const getPathFile = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getPathFile(filename), 'utf-8');
 
-test('diffirence with two flat json files', () => {
-  expect(genDiff(getPathFile('filepath1.json'), getPathFile('filepath2.json')))
-    .toEqual(result);
+const beforeJson = getPathFile('filepath1.json');
+const afterJson = getPathFile('filepath2.json');
+const beforeYaml = getPathFile('filepath1.yaml');
+const afterYaml = getPathFile('filepath2.yaml');
+const beforeYml = getPathFile('filepath1.yml');
+const afterYml = getPathFile('filepath2.yml');
+
+const resultStylish = readFile('stylish.txt');
+
+test('diffirence with two json files', () => {
+  expect(genDiff(beforeJson, afterJson))
+    .toEqual(resultStylish);
 });
 
-test('diffirence with two flat yaml files', () => {
-  expect(genDiff(getPathFile('filepath1.yaml'), getPathFile('filepath2.yaml')))
-    .toEqual(result);
-  expect(genDiff(getPathFile('filepath1.yml'), getPathFile('filepath2.yml')))
-    .toEqual(result);
+test('diffirence with two yaml files', () => {
+  expect(genDiff(beforeYaml, afterYaml))
+    .toEqual(resultStylish);
+  expect(genDiff(beforeYml, afterYml))
+    .toEqual(resultStylish);
 });
