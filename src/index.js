7811a047daa9a9e14d522a3d645b4e5e366180f6
DarkJunior <sfp87@protonmail.com>
2021-06-22 19:18:05 +0300

    refactor code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
+import * as fs from 'fs';
+import path from 'path';
 import genDiff from './fileComparison.js';
 import makeDiff from './formatters/index.js';
 import parse from './parse.js';
 
+const extName = (fileName) => path.extname(fileName);
+const filePathForParse = (fileName) => fs.readFileSync(path.resolve(`${process.cwd()}`, `${fileName}`), 'utf-8');
+
 export default (filePath1, filePath2, formatterName = 'stylish') => {
-  const before = parse(filePath1);
-  const after = parse(filePath2);
+  const extNameBefore = extName(filePath1);
+  const extNameAfter = extName(filePath2);
+  const dataBefore = filePathForParse(filePath1);
+  const dataAfter = filePathForParse(filePath2);
+  const before = parse(extNameBefore, dataBefore);
+  const after = parse(extNameAfter, dataAfter);
   const diff = genDiff(before, after);
   return makeDiff(diff, formatterName);
 };
--- src/parse.js
+++ src/parse.js
@@ -1,18 +1,12 @@
-import * as fs from 'fs';
-
-import path from 'path';
-
 import yaml from 'js-yaml';
 
-export default (fileName) => {
-  const extName = path.extname(fileName);
-  const filePathForParse = fs.readFileSync(path.resolve(`${process.cwd()}`, `${fileName}`), 'utf-8');
+export default (extName, data) => {
   switch (extName) {
     case '.json':
-      return JSON.parse(filePathForParse);
+      return JSON.parse(data);
     case '.yaml':
     case '.yml':
-      return yaml.load(filePathForParse);
+      return yaml.load(data);
     default:
       throw new Error(`Unknown file format: ${extName}`);
   }
